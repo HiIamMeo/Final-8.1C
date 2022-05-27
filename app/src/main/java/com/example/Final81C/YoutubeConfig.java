9f4e33c6d3de899ddaffa7c9d9c913ad94c7bd7b
Truong Phuc Le <lephuc1752001@gmail.com>
2022-05-27 23:33:34 +1000

    Initial commit

---
+++ app/src/main/java/com/example/Final81C/YoutubeConfig.java
@@ -0,0 +1,13 @@
+package com.example.Final81C;
+
+public class YoutubeConfig {
+
+    public YoutubeConfig() {
+    }
+
+    private static final String API_KEY = "AIzaSyDzDfDsnRJ0iUayUQGBGa_-jGG-bj7Qe6o";
+
+    public static String getApiKey() {
+        return API_KEY;
+    }
+}
